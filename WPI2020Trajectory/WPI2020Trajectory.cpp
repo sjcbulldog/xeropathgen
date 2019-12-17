a4f3832f755d215ce10ba0febd68ac281aea2d54
Jack (Butch) Griffin <butchg@comcast.net>
2019-12-17 08:04:35 -0800

    started adding WPILIB 2020 trajectory generator

---
+++ WPI2020Trajectory/WPI2020Trajectory.cpp
@@ -0,0 +1,10 @@
+// WPI2020Trajectory.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <frc/trajectory/TrapezoidProfile.h>
+#include <frc/trajectory/TrajectoryGenerator.h>
+#include <iostream>
+
+int main()
+{
+}
